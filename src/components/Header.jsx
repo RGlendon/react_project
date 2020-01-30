146280c3d3985ecee03d74a1e9de18ed2b2e8f2d
RGlendon <Rold.Glendon@gmail.com>
2020-01-30 20:44:11 +0300

    initialization

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="logo" src="https://image.flaticon.com/icons/svg/908/908097.svg" />
+    </header>
+  );
+}
+
+export default Header;
