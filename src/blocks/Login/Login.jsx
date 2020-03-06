3e57c31531df63c6962e0264ec3258d6f04e3457
RGlendon <Rold.Glendon@gmail.com>
2020-03-07 01:46:34 +0300

    add redux-form

--- src/blocks/Login/Login.jsx
+++ src/blocks/Login/Login.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
+
+let LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component={'input'} type="text" name="login" placeholder={'your login'}/>
+            </div>
+            <div>
+                <Field component={'input'} type="password" name="enter password" placeholder={'password'}/>
+            </div>
+            <div>
+                <Field component={'input'} type="checkbox" name="rememberMe"/> Remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    );
+};
+
+LoginForm = reduxForm({form: 'loginForm'})(LoginForm);
 
 const Login = (props) => {
+    const onSubmit= (formData) => {
+        console.log(formData);
+    };
+
     return (
-        <p>Login</p>
+        <div>
+            <h1>Login</h1>
+            <LoginForm onSubmit={onSubmit}/>
+        </div>
     );
 }
 
