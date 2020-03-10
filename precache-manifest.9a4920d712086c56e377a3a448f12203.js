a2170fa542f33b4c13e20ad9add70298f258c2ed
RGlendon <Rold.Glendon@gmail.com>
2020-03-11 00:38:01 +0300

    Updates

---
+++ precache-manifest.9a4920d712086c56e377a3a448f12203.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9089f91a680333f41d74ee6f05265a2c",
+    "url": "/react_project/index.html"
+  },
+  {
+    "revision": "6b5807489269ad12535e",
+    "url": "/react_project/static/css/3.7c046c50.chunk.css"
+  },
+  {
+    "revision": "71996ef42455197cad3d",
+    "url": "/react_project/static/css/main.c1b2f36c.chunk.css"
+  },
+  {
+    "revision": "ec223bbb45c851ec3c45",
+    "url": "/react_project/static/js/2.848f4d8c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react_project/static/js/2.848f4d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5807489269ad12535e",
+    "url": "/react_project/static/js/3.f8db9894.chunk.js"
+  },
+  {
+    "revision": "71996ef42455197cad3d",
+    "url": "/react_project/static/js/main.1e96f90e.chunk.js"
+  },
+  {
+    "revision": "6b9573916abe12ea66e1",
+    "url": "/react_project/static/js/runtime-main.c7dda5ef.js"
+  },
+  {
+    "revision": "0fba2a312c3753b7f18c72e3bdc99693",
+    "url": "/react_project/static/media/preloader.0fba2a31.gif"
+  }
+]);
